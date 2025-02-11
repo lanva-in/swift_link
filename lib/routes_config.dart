d8b14f5dac87b9034ea86f55cc290219fc228cca
hedongyang <contact@lanva-in.com>
2025-02-11 18:13:58 +0800

    搭框架页面

---
+++ lib/routes_config.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+
+class RouteConstants {
+
+  ///主页面
+  static const home = "/app/home";
+
+  ///锁页面
+  static const lock = "/app/lock";
+
+  ///快捷页面
+  static const shortcut = "/app/shortcut";
+
+}
+
+
+class RouteConfig {
+
+  static List<GetPage> routes() {
+    return [
+
+    ];
+  }
+
+}
